bd560b6c0cc9ac4dff8c3610cb5fd836b03ac61e
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-02-26 02:39:42 +0900

    dd

--- pages/authmessage/[phonenumber].tsx
+++ pages/authmessage/[phonenumber].tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from "next/router";
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 export default function message() {
     const router = useRouter();
     const [phoneValue, setPhoneValue] = useState<string>("");
@@ -14,7 +14,7 @@ export default function message() {
 
     const disabledRepeat = phoneValue.length !== 11;
     const disabledAuth = auth.length === 0;
-   
+
     const handleChange = (evt: React.ChangeEvent<HTMLInputElement>, index: number) => {
         if (index === 1 && evt.target.value.length <= 11) { setPhoneValue(evt.target.value) };
         if (index === 2) { setAuth(evt.target.value); }
@@ -22,7 +22,7 @@ export default function message() {
     const handleAuthClick = () => {
         console.log(auth);
         if (auth === "sunok loves wonseob") {
-            console.log("right");
+            router.push('/main')
         }
         else {
             setAuthErrorHidden(false);
@@ -43,15 +43,28 @@ export default function message() {
                     </div>
 
                     <div className='flex flex-col'>
-                        <input type="text" placeholder={phoneValue} value={phoneValue} onChange={(evt) => { handleChange(evt, 1) }} className='px-3 text-xl my-4 bg-transparent border-solid border-2 border-[#C0C0C0] py-2 text-[#C0C0C0] rounded-lg' />
-                        <button disabled={disabledRepeat} className={`rounded-lg border-solid border-2 py-3 text-2xl ${!disabledRepeat ? `border-[#C0C0C0] text-[#C0C0C0] font-bold` : `border-slate-600 text-slate-600`}`}>인증문자 다시 받기</button>
-                        <input type="text" placeholder="인증번호를 입력해주세요" value={auth} onChange={(evt) => { handleChange(evt, 2) }} className="rounded-lg px-3 text-xl my-4 bg-transparent border-solid border-2 border-[#C0C0C0] py-2 text-[#C0C0C0]" />
+                        <input type="text"
+                            placeholder={phoneValue}
+                            value={phoneValue}
+                            onChange={(evt) => { handleChange(evt, 1) }}
+                            className='px-3 text-xl my-4 bg-transparent border-solid border-2 border-[#C0C0C0] py-2 text-[#C0C0C0] rounded-lg' />
+
+                        <button disabled={disabledRepeat}
+                            className={`rounded-lg border-solid border-2 py-3 text-2xl 
+                            ${!disabledRepeat ? `border-[#C0C0C0] text-[#C0C0C0] font-bold` : `border-slate-600 text-slate-600`}`}>
+                            인증문자 다시 받기</button>
+                        <input type="text" placeholder="인증번호를 입력해주세요"
+                            value={auth} onChange={(evt) => { handleChange(evt, 2) }}
+                            className="rounded-lg px-3 text-xl my-4 bg-transparent border-solid border-2 border-[#C0C0C0] py-2 text-[#C0C0C0]" />
                         <p className="font-normal text-lg text-[#C0C0C0]">어떤 경우에도 타인에게 공유하지 마세요!</p>
-                        <button disabled={disabledAuth} onClick={handleAuthClick} className={`py-3 mt-2 rounded-lg py-2 text-2xl text-[#C0C0C0] font-bold ${!disabledAuth ? `bg-[#ff6f0f]` : ` bg-slate-600`}`}>인증번호 확인</button>
+                        <button disabled={disabledAuth}
+                            onClick={handleAuthClick}
+                            className={`py-3 mt-2 rounded-lg py-2 text-2xl text-[#C0C0C0] font-bold 
+                            ${!disabledAuth ? `bg-[#ff6f0f]` : ` bg-slate-600`}`}>인증번호 확인</button>
                     </div>
                 </div>
             </div>
-            <div hidden={authErrorHidden} className="p-2 bg-[#C0C0C0] text-lg">
+            <div hidden={authErrorHidden} className="p-10 bg-[#C0C0C0] text-lg rounded-lg">
                 전화번호가 잘못되었어요. 다시 한번 확인해주세요.
             </div>
         </div>
---
+++ pages/searchplace.tsx
@@ -0,0 +1,5 @@
+export default function searchplace(){
+    return (
+        <div>dd</div>
+    )
+}
